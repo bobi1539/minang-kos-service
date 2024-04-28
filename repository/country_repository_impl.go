45b785253a97c0338848275445f0fd36c8329da6
bobi.rival <bahmadrival@gmail.com>
2024-04-28 09:29:59 +0700

    Add more crud function in country repository

--- repository/country_repository_impl.go
+++ repository/country_repository_impl.go
@@ -3,6 +3,8 @@ package repository
 import (
 	"context"
 	"database/sql"
+	"errors"
+	"minang-kos-service/constant"
 	"minang-kos-service/helper"
 	"minang-kos-service/model/domain"
 )
@@ -15,11 +17,11 @@ func NewCountryRepository() CountryRepository {
 }
 
 func (repository *CountryRepositoryImpl) Save(ctx context.Context, tx *sql.Tx, domainModel any) any {
-	SQL := "insert into m_country(name, created_at, created_by, created_by_name, " +
+	sqlQuery := "insert into m_country(name, created_at, created_by, created_by_name, " +
 		"updated_at, updated_by, updated_by_name, is_deleted) values (?,?,?,?,?,?,?,?)"
 
 	country := domainModel.(domain.Country)
-	result, err := tx.ExecContext(ctx, SQL, country.Name, country.CreatedAt, country.CreatedBy, country.CreatedByName,
+	result, err := tx.ExecContext(ctx, sqlQuery, country.Name, country.CreatedAt, country.CreatedBy, country.CreatedByName,
 		country.UpdatedAt, country.UpdatedBy, country.UpdatedByName, country.IsDeleted)
 	helper.PanicIfError(err)
 
@@ -29,4 +31,108 @@ func (repository *CountryRepositoryImpl) Save(ctx context.Context, tx *sql.Tx, d
 	country.Id = id
 
 	return country
+}
+
+func (repository *CountryRepositoryImpl) Update(ctx context.Context, tx *sql.Tx, domainModel any) any {
+	sqlQuery := "update m_country set name = ?, updated_at = ?, updated_by = ?, updated_by_name = ? where id = ?"
+
+	country := domainModel.(domain.Country)
+	_, err := tx.ExecContext(ctx, sqlQuery, country.Name, country.UpdatedAt, country.UpdatedBy, country.UpdatedByName, country.Id)
+	helper.PanicIfError(err)
+
+	return country
+}
+
+func (repository *CountryRepositoryImpl) Delete(ctx context.Context, tx *sql.Tx, domainModel any) {
+	sqlQuery := "update m_country set updated_at = ?, updated_by = ?, updated_by_name = ?, is_deleted = ? where id = ?"
+
+	country := domainModel.(domain.Country)
+	_, err := tx.ExecContext(ctx, sqlQuery, country.UpdatedAt, country.UpdatedBy, country.UpdatedByName, true, country.Id)
+	helper.PanicIfError(err)
+}
+
+func (repository *CountryRepositoryImpl) FindById(ctx context.Context, tx *sql.Tx, id int64) (any, error) {
+	sqlQuery := "select id, name, created_at, created_by, created_by_name, " +
+		"updated_at, updated_by, updated_by_name, is_deleted from m_country where id = ?"
+
+	rows, err := tx.QueryContext(ctx, sqlQuery, id)
+	helper.PanicIfError(err)
+	defer rows.Close()
+
+	country := domain.Country{}
+
+	if rows.Next() {
+		scanCountry(rows, country)
+		return country, nil
+	}
+	return country, errors.New(constant.DATA_NOT_FOUND)
+}
+
+func (repository *CountryRepositoryImpl) FindAllWithPagination(ctx context.Context, tx *sql.Tx, searchBy map[any]any) any {
+	name := searchBy["name"]
+	page := searchBy["page"]
+	size := searchBy["size"]
+
+	page = page.(int) - 1
+
+	sqlQuery := "select id, name, created_at, created_by, created_by_name, updated_at, updated_by, updated_by_name, is_deleted" +
+		" from m_country"
+	if name != nil {
+		sqlQuery += " where name like %" + name.(string) + "%"
+	}
+	sqlQuery += " limit " + size.(string) + " offset " + page.(string)
+	sqlQuery += " order by id asc"
+
+	var rows *sql.Rows
+	var err error
+
+	if name != nil {
+		rows, err = tx.QueryContext(ctx, sqlQuery, name, size, page)
+	} else {
+		rows, err = tx.QueryContext(ctx, sqlQuery, size, page)
+	}
+	helper.PanicIfError(err)
+	defer rows.Close()
+
+	return getCountries(rows)
+}
+
+func (repository *CountryRepositoryImpl) FindAllWithoutPagination(ctx context.Context, tx *sql.Tx, searchBy map[any]any) any {
+	name := searchBy["name"]
+
+	sqlQuery := "select id, name, created_at, created_by, created_by_name, updated_at, updated_by, updated_by_name, is_deleted" +
+		" from m_country"
+	if name != nil {
+		sqlQuery += " where name like %" + name.(string) + "%"
+	}
+	sqlQuery += " order by id asc"
+
+	var rows *sql.Rows
+	var err error
+
+	if name != nil {
+		rows, err = tx.QueryContext(ctx, sqlQuery, name)
+	} else {
+		rows, err = tx.QueryContext(ctx, sqlQuery)
+	}
+	helper.PanicIfError(err)
+	defer rows.Close()
+
+	return getCountries(rows)
+}
+
+func scanCountry(rows *sql.Rows, country domain.Country) {
+	err := rows.Scan(&country.Id, &country.Name, &country.CreatedAt, &country.CreatedBy, &country.CreatedByName,
+		&country.UpdatedAt, &country.UpdatedBy, &country.UpdatedByName, &country.IsDeleted)
+	helper.PanicIfError(err)
+}
+
+func getCountries(rows *sql.Rows) []domain.Country {
+	var countries []domain.Country
+	for rows.Next() {
+		country := domain.Country{}
+		scanCountry(rows, country)
+		countries = append(countries, country)
+	}
+	return countries
 }
--- repository/crud_repository.go
+++ repository/crud_repository.go
@@ -7,4 +7,9 @@ import (
 
 type CrudRepository interface {
 	Save(ctx context.Context, tx *sql.Tx, domainModel any) any
+	Update(ctx context.Context, tx *sql.Tx, domainModel any) any
+	Delete(ctx context.Context, tx *sql.Tx, domainModel any)
+	FindById(ctx context.Context, tx *sql.Tx, id int64) (any, error)
+	FindAllWithPagination(ctx context.Context, tx *sql.Tx, searchBy map[any]any) any
+	FindAllWithoutPagination(ctx context.Context, tx *sql.Tx, searchBy map[any]any) any
 }
